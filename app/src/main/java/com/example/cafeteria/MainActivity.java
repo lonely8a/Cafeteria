54274f25a23dfc2fe93f9a6986bb0365371d9c78
lonely8a <20242d047@khipu.edu.pe>
2026-05-24 18:50:43 -0500

    Agregué pantallas XML

---
+++ app/src/main/java/com/example/cafeteria/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cafeteria;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    MainActivity.this,
+                    SegundaActivity.class
+            );
+
+            startActivity(intent);
+
+        });
+    }
+}
