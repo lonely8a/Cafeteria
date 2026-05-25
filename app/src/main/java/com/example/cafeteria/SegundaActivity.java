9623bafcaa7a9da4e0dad789ae81011ecaec955e
Max8246 <maxbrando8246@gmail.com>
2026-05-25 00:13:10 -0500

    login primera07

--- app/src/main/java/com/example/cafeteria/SegundaActivity.java
+++ app/src/main/java/com/example/cafeteria/SegundaActivity.java
@@ -2,23 +2,47 @@ package com.example.cafeteria;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SegundaActivity extends  AppCompatActivity {
+    Button iniciarsesion;
+    TextView sincuenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_segunda);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        iniciarsesion = findViewById(R.id.iniciarsesion);
+
+        sincuenta = findViewById(R.id.sincuenta);
+
+        // BOTON INICIAR SESION -> ACTIVITY_CUARTO
+        iniciarsesion.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    SegundaActivity.this,
+                    cuartoActivity.class
+            );
+
+            startActivity(intent);
+
+        });
+
+        // TEXTO CREAR UNA CUENTA -> ACTIVITY_TERCERA
+        sincuenta.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    SegundaActivity.this,
+                    TerceraActivity.class
+            );
+
+            startActivity(intent);
         });
     }
 }
